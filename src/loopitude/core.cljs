456557b6c76765859ff7b7a2333ce6c8708f696d
j201 <j201.alex@gmail.com>
2014-10-27 12:56:55 -0400

    Add tempo slider

--- src/loopitude/core.cljs
+++ src/loopitude/core.cljs
@@ -8,14 +8,20 @@
 (defn player []
   (let [playing (reagent/atom false)
         note-no (reagent/atom 0)
+        tempo (reagent/atom 100)
         play-click (fn []
                      (reset! playing true)
-                     (synth/play @notes 0 200))]
+                     (synth/play @notes 0 @tempo))]
     (fn []
       [:div
        [piano-roll {:notes notes, :row-offset 0, :playing-col (if @playing @note-no nil)}]
        [:button {:on-click play-click}
-        "Play"]])))
+        "Play"]
+       [:input {:type "range"
+                :min "50"
+                :max "500"
+                :step "10"
+                :on-change #(reset! tempo (-> % .-target .-value))}]])))
 
 (reagent/render-component [player]
                           (.-body js/document))
