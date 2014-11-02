f6abc96725e432461f584213a703c42b158c7954
Alex <j201.alex@gmail.com>
2014-11-02 18:23:18 -0500

    Speed up piano roll

--- src/loopitude/piano-roll.cljs
+++ src/loopitude/piano-roll.cljs
@@ -3,17 +3,13 @@
 
 (def rows 25)
 (def default-row-offset (- (.ceil js/Math (/ rows 2)))) ;; 0 is middle C, centered at 0
-(def cols 16)
+(def cols 32)
 
-(defn log [x]
-  (.log js/console (clj->js x))
-  x)
-
-(defn- cell [{:keys [on playing middle on-mouse]}]
+(defn- cell [{:keys [on playing on-mouse toggle-note col row-note]}]
   [:td {:class (str (when on "on ")
                     (when playing "playing ")
-                    (when middle "middle"))
-        :on-mouse-down on-mouse}])
+                    (when (= 0 row-note) "middle"))
+        :on-mouse-down #(do (.log js/console "click") (toggle-note col row-note))}])
 
 (defn- piano-row [{:keys [row-note notes toggle-note playing-col]}]
   [:tr (for [col (range cols)
@@ -21,23 +17,26 @@
          ^{:key col}
          [cell {:on on
                 :playing (= col playing-col)
-                :middle (= 0 row-note)
-                :on-mouse #(toggle-note col row-note)}])])
+                :col col
+                :row-note row-note
+                :toggle-note toggle-note}])])
 
-(defn piano-roll [{:keys [notes row-offset playing-col]}]
-  (let [notes' @notes
-        row-offset' @row-offset]
-    [:table.piano-roll
-     (for [row (range rows)
-           :let [row-note (+ (- rows row) default-row-offset row-offset')]]
-       ^{:key row}
-       [piano-row {:row-note row-note
-                   :notes (set (filter #(contains? (notes' %) row-note) (range cols)))
-                   :playing-col playing-col
-                   :toggle-note (fn [col row-note]
-                                  (swap! notes
-                                         (fn [notes]
-                                           (update-in notes [col]
-                                                                #((if (contains? (notes col) row-note) disj conj)
-                                                                  (or % #{})
-                                                                  row-note)))))}])]))
+(defn piano-roll [{:keys [notes]}]
+  (let [toggle-note (fn [col row-note]
+                      (swap! notes
+                             (fn [notes]
+                               (update-in notes [col]
+                                          #((if (contains? (notes col) row-note) disj conj)
+                                            (or % #{})
+                                            row-note)))))]
+    (fn [{:keys [notes row-offset playing-col]}]
+      (let [notes' @notes
+            row-offset' @row-offset]
+        [:table.piano-roll
+         (for [row (range rows)
+               :let [row-note (+ (- rows row) default-row-offset row-offset')]]
+           ^{:key row}
+           [piano-row {:row-note row-note
+                       :notes (set (filter #(contains? (notes' %) row-note) (range cols)))
+                       :playing-col playing-col
+                       :toggle-note toggle-note}])]))))
